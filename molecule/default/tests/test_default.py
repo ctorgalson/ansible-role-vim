2ce8126f6c100a49dc9254b51d1ebb4478a0dd0d
Christopher Torgalson <manager@bedlamhotel.com>
2019-09-28 21:21:06 +0200

    Set up initial tests for role

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,26 @@
 import os
 
+import pytest
+
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize("package", [
+    "vim.tiny",
+])
+def test_removed_vim_packages(host, package):
+    f = host.file("/usr/bin/{}".format(package))
+
+    assert not f.exists
+
+
+@pytest.mark.parametrize("package", [
+    "vim",
+])
+def test_installed_vim_packages(host, package):
+    f = host.file("/usr/bin/{}".format(package))
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
